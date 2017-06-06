955536551a9b9ba4e9d8863f8e2773595dae9b3e
SuHang_Web <SuHang_Web@163.com>
2017-06-06 11:38:08 +0800

    德商博锐

--- deshang/js/style.js
+++ deshang/js/style.js
@@ -8,7 +8,7 @@ $(window).resize(function() {
 function NavShow(){
     $('.Nav-Wra').toggleClass('active');
     $('.Nav-content-Nei').removeClass('active');
-    $('.Nav-Wra .Nav ul li a.Click-Nav').removeClass('active');
+    // $('.Nav-Wra .Nav ul li a.Click-Nav').removeClass('active');
 };
 
 //二级导航菜单
@@ -27,6 +27,11 @@ $('.Nav-Wra .Nav ul li a.Click-Nav').click(function(){
 $('.Content_4_click').click(function () {
     $('.Nav-Wra').addClass('active');
     $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top + "px"}, 800);
+    return false;//不要这句会有点卡顿
+});
+//首页第一屏按钮
+$('.home-bottom-Arrow').click(function(){
+	$("html, body").animate({scrollTop: $($(this).attr("href")).offset().top + "px"}, 800);
     return false;//不要这句会有点卡顿
 });
 
@@ -86,46 +91,6 @@ $(".header .container-fluid .navbar-toggle").click(function(event) {
 
 
 
-/*返回顶部 开始*/
-    function myEvent(obj,ev,fn){
-    if(obj.attachEvent){
-        obj.attachEvent('on'+ev,fn);
-    }else{
-        obj.addEventListener(ev,fn,false);
-    }
-}
-myEvent(window,'load',function(){
-
-    var oRTT=document.getElementById('rtt');
-    var sho=document.getElementById('sho');
-    var pH=document.documentElement.clientHeight;
-    var timer=null;
-    var scrollTop;
-    window.onscroll=function(){
-        scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
-        if(scrollTop>=pH-500){
-            $('#sho').show();
-        }else{
-            $('#sho').hide();
-        }
-        return scrollTop;
-    };
-    $('#rtt').click(function(){
-        clearInterval(timer);
-        timer=setInterval(function(){
-            var now=scrollTop;
-            var speed=(0-now)/10;
-            speed=speed>0?Math.ceil(speed):Math.floor(speed);
-            if(scrollTop==0){
-                clearInterval(timer);
-            }
-            document.documentElement.scrollTop=scrollTop+speed;
-            document.body.scrollTop=scrollTop+speed;
-        }, 30);
-    });
-});
-/*返回顶部 结束*/
-
 //页面动画
 function animation(obj, animate) {//两个参数 第一个是时间 第二个是动画方式
     var sh = $(document).scrollTop(); //滚动条高度
@@ -149,6 +114,44 @@ function animation(obj, animate) {//两个参数 第一个是时间 第二个是
         });
     })
 }
+
+
+$('.Product-Div-B .Click-Top .f-r a').eq(0).click(function(){
+    $('.Product-Div-B .content-1 ul li:first-child .f-r').html('<span>NORMA</span><span>NOHARD A</span><span>HARDNORMA</span>');
+    $('.Product-Div-B .content-1 ul li:last-child .f-r').html('<span>NORMA</span><span>NOHARD A</span>');
+    $('.Product-Div-B .content-2 .se div').width('30%');
+    $('.Product-Div .f-r.width > img').attr('src','images/p-11-1.jpg');
+    $('.Product-Div-B .content-3 ul li .f-r p').html('DIN ISO 7619 / DIN 3213 ISO 868<br />DIN ISO 7619 / 3213 EN ISO 868');
+    $('.Product-Div-B .content-3 ul li .f-r dl dt img').attr('src','images/p-2-1.jpg');
+});
+$('.Product-Div-B .Click-Top .f-r a').eq(1).click(function(){
+    $('.Product-Div-B .content-1 ul li:first-child .f-r').html('<span>NORMA21232</span><span>32131 A</span><span>32132</span>');
+    $('.Product-Div-B .content-1 ul li:last-child .f-r').html('<span>NORMA</span><span>NOHARD A</span><span>NOHARD A</span>');
+    $('.Product-Div-B .content-2 .se div').width('50%');
+    $('.Product-Div .f-r.width > img').attr('src','images/p-11-2.jpg');
+    $('.Product-Div-B .content-3 ul li .f-r p').html('DIN ISO 7619 / DIN 212454 ISO 868<br />32132 ISO 7619 / 3213 EN ISO 868');
+    $('.Product-Div-B .content-3 ul li .f-r dl dt img').attr('src','images/p-2-2.jpg');
+});
+$('.Product-Div-B .Click-Top .f-r a').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
